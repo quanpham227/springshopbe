3d80de924af26d6b199d762ba51ac9b85e68e455
quan-pham <hongquanqtqb@gmail.com>
2023-04-16 21:23:38 +0700

    add download logo for manufacturer

--- src/main/java/com/springshopbe/controller/ManufacturerController.java
+++ src/main/java/com/springshopbe/controller/ManufacturerController.java
@@ -1,20 +1,22 @@
 package com.springshopbe.controller;
 
 import com.springshopbe.dto.ManufacturerDTO;
+import com.springshopbe.exeption.FileNotFoundExeption;
+import com.springshopbe.exeption.FileStogareExeption;
 import com.springshopbe.service.IManufacturerServicer;
 import com.springshopbe.service.impl.FileStogareService;
 import com.springshopbe.service.impl.ManufacturerService;
 import com.springshopbe.service.impl.MapValidationErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RestController
@@ -38,5 +40,25 @@ public class ManufacturerController {
         }
         ManufacturerDTO dto = manufacturerService.insertManufacturer(manufacturerDTO);
         return new  ResponseEntity<>(dto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/logo/{filename:.+}")
+    public ResponseEntity<?> downloadFile (@PathVariable String filename, HttpServletRequest request){
+        Resource resource = fileStogareService.loadFileAsResource(filename);
+
+        String contentType = null;
+        try{
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        }catch (Exception exception) {
+            throw new FileStogareExeption("Could not determine file type");
+        }
+        if(contentType == null){
+            contentType = "application/octet-stream";
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=\""
+                        + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
